ea86fa3819b83ce8a5c3c0eef246df3fabf340ad
Nicolas <nicolas.jaracarvajal@utem.cl>
2023-11-24 23:57:51 -0300

    test

--- config/auth.js
+++ config/auth.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: "https://rest-dev-xnkc.4.us-1.fl0.io/auth/google/callback",
   passReqToCallback: true
 },
   async function (request, accessToken, refreshToken, profile, done) {
