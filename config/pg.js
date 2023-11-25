1f4f21cbdc0944722c11c67eccccea3d7c5e76f4
Nicolas <nicolas.jaracarvajal@utem.cl>
2023-11-24 22:55:56 -0300

    Cambio variables de entorno

--- config/pg.js
+++ config/pg.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
+require("dotenv").config();
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'motty.db.elephantsql.com',
-  database: 'ftsomtvd',
-  username: 'ftsomtvd',
-  password: 'lqeSpoLls3xNAB8qlxSLWvT2yXsWz-RJ',
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
   port: 5432,
 });
 
